1b0574f4a7dc91ef9c860cd960343f3243ffdeb3
Raphael <oliveira_raphael@msn.com>
2024-09-13 16:56:58 -0300

    add updates

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express from 'express';
 import { processCSVData } from './services/csvService';
 import { formatToBRL, validarPrestacoes, validateCNPJ, validateCPF } from './utils/validation';
+import { paginate } from './utils/pagination';
+import { InvalidResult, ValidResult } from './types';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/process-csv', async (req, res) => {
   try {
+    const { page = 1, pageSize = 10, onlyValid = 'false' } = req.query;
     const data = await processCSVData('data.csv');
+
+    const validResults: ValidResult[] = [];
+    const invalidResults: InvalidResult[] = [];
 
     data.forEach(row => {
       const { nrCpfCnpj, vlTotal, qtPrestacoes, vlPresta } = row;
@@ -16,7 +22,7 @@ app.get('/process-csv', async (req, res) => {
       const isCNPJ = nrCpfCnpj.length === 14 ? validateCNPJ(nrCpfCnpj) : false;
 
       if (!isCPF && !isCNPJ) {
-        console.log(`CPF ou CNPJ inválido: ${nrCpfCnpj}`);
+        invalidResults.push({ nrCpfCnpj, error: 'CPF ou CNPJ inválido' });
       }
 
       const total = parseFloat(vlTotal);
@@ -24,21 +30,43 @@ app.get('/process-csv', async (req, res) => {
       const valorPresta = parseFloat(vlPresta);
 
       if (!validarPrestacoes(total, prestacoes, valorPresta)) {
-        console.log(`Inconsistência nos valores de prestações para ${nrCpfCnpj}`);
+        invalidResults.push({ nrCpfCnpj, error: 'Inconsistência nos valores de prestações' });
       }
 
       const totalFormatado = formatToBRL(total);
       const valorPrestaFormatado = formatToBRL(valorPresta);
 
-      console.log(`CPF/CNPJ: ${nrCpfCnpj} | Total: ${totalFormatado} | Prestação: ${valorPrestaFormatado}`);
+      validResults.push({
+        nrCpfCnpj,
+        vlTotal: totalFormatado,
+        qtPrestacoes,
+        vlPresta: valorPrestaFormatado,
+      });
     });
+    const sortedResults = onlyValid === 'true' ? validResults : [...validResults, ...invalidResults];
 
-    res.send('Processamento de CSV concluído com sucesso.');
+    const pageInt = parseInt(page as string, 10);
+    const pageSizeInt = parseInt(pageSize as string, 10);
+    const paginatedResults = paginate(sortedResults, pageInt, pageSizeInt);
+
+    const totalItems = sortedResults.length;
+    const totalPages = Math.ceil(totalItems / pageSizeInt);
+
+    res.json({
+      data: paginatedResults,
+      metadata: {
+        totalItems,
+        totalPages,
+        currentPage: pageInt,
+        pageSize: pageSizeInt,
+      },
+    });
   } catch (error) {
-    res.status(500).send('Erro ao processar CSV');
+    res.status(500).json({ error: 'Erro ao processar CSV' });
   }
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
