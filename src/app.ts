7c468fd02dc7a1a9c3b11946aa55cf920f55f73b
Raphael <oliveira_raphael@msn.com>
2024-09-13 14:37:04 -0300

    use validator on date

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,39 @@
 import express from 'express';
 import { processCSVData } from './services/csvService';
+import { formatToBRL, validarPrestacoes, validateCNPJ, validateCPF } from './utils/validation';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/process-csv', async (req, res) => {
   try {
-    await processCSVData();
-    res.send('CSV Processado com sucesso!');
+    const data = await processCSVData('data.csv');
+
+    data.forEach(row => {
+      const { nrCpfCnpj, vlTotal, qtPrestacoes, vlPresta } = row;
+
+      const isCPF = nrCpfCnpj.length === 11 ? validateCPF(nrCpfCnpj) : false;
+      const isCNPJ = nrCpfCnpj.length === 14 ? validateCNPJ(nrCpfCnpj) : false;
+
+      if (!isCPF && !isCNPJ) {
+        console.log(`CPF ou CNPJ inválido: ${nrCpfCnpj}`);
+      }
+
+      const total = parseFloat(vlTotal);
+      const prestacoes = parseInt(qtPrestacoes);
+      const valorPresta = parseFloat(vlPresta);
+
+      if (!validarPrestacoes(total, prestacoes, valorPresta)) {
+        console.log(`Inconsistência nos valores de prestações para ${nrCpfCnpj}`);
+      }
+
+      const totalFormatado = formatToBRL(total);
+      const valorPrestaFormatado = formatToBRL(valorPresta);
+
+      console.log(`CPF/CNPJ: ${nrCpfCnpj} | Total: ${totalFormatado} | Prestação: ${valorPrestaFormatado}`);
+    });
+
+    res.send('Processamento de CSV concluído com sucesso.');
   } catch (error) {
     res.status(500).send('Erro ao processar CSV');
   }
